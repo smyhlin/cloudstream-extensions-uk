0f834e038ae2da0ea2ea89537d90e9a75a4cd323
Sergii <37741855+smyhlin@users.noreply.github.com>
2025-08-11 15:31:07 +0300

    Update AshdiExtractor.kt
    Цей файл змінено для стандартизації. Він тепер повертає просту назву "Ashdi", а провайдер додає до неї назву озвучення.

--- AnitubeinuaProvider/src/main/kotlin/com/lagradost/extractors/AshdiExtractor.kt
+++ AnitubeinuaProvider/src/main/kotlin/com/lagradost/extractors/AshdiExtractor.kt
@@ -1,12 +1,30 @@
-package com.lagradost.extractors;
+package com.lagradost.extractors
 
 import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.getQualityFromName
 
-class AshdiExtractor() {
-    suspend fun ParseM3U8(url: String): String{
-        return app.get(url).document.select("script").html()
-            .substringAfterLast("file:\"")
-            .substringBefore("\",")
+open class Ashdi : ExtractorApi() {
+    override var name = "Ashdi"
+    override var mainUrl = "https://ashdi.vip"
+    override val requiresReferer = true
 
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
+        val response = app.get(url, referer = referer).document
+        val m3u8Url = response.select("script").html().let {
+            Regex("file:\"(.*?)\"").find(it)?.groupValues?.get(1)
+        } ?: return null
+
+        return listOf(
+            ExtractorLink(
+                this.name, // Назва екстрактора
+                this.name, // Назва джерела (буде доповнена в провайдері)
+                m3u8Url,
+                referer ?: mainUrl,
+                getQualityFromName(""),
+                isM3u8 = true
+            )
+        )
     }
 }
